7277e03093bc5d88daccc22b01c9d65de1b1c53c
Haotao Lai <h_lai@miami.encs.concordia.ca>
2019-06-18 02:04:43 -0400

    adapt to viray

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from yolo3.utils import get_random_data
 
 def _main():
     annotation_path = '2012_train.txt'            # train.txt
-    log_dir = 'logs/000/'
+    log_dir = 'logs/virya/'
     classes_path = 'model_data/voc_person.txt'    # define different class of object
     anchors_path = 'model_data/yolo_anchors.txt'  # anchor configuration file
     class_names = get_classes(classes_path)
@@ -29,14 +29,8 @@ def _main():
         model = create_tiny_model(input_shape, anchors, num_classes,
             freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
-        # model = create_model(input_shape, anchors, num_classes,
-        #     freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
-
-        # **********************************************************************************************************
-        # modify as follow to continue training after the first training part is done below
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path= log_dir+'trained_weights_stage_1.h5') # make sure you know what you freeze
-        # **********************************************************************************************************
+            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
@@ -55,7 +49,7 @@ def _main():
 
     # Train with frozen layers first, to get a stable loss.
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
-    if False:
+    if True:
         model.compile(optimizer=Adam(lr=1e-3), loss={
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
@@ -79,7 +73,7 @@ def _main():
         model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
-        batch_size = 8 # note that more GPU memory is required after unfreezing the body
+        batch_size = 32 # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
             steps_per_epoch=max(1, num_train//batch_size),
--- voc_annotation.py
+++ voc_annotation.py
@@ -9,7 +9,7 @@ sets=[('2012', 'train'), ('2012', 'val')]
 classes = ["person"]
 
 def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    in_file = open('../datasets/VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
     tree=ET.parse(in_file)
     root = tree.getroot()
 
@@ -26,10 +26,10 @@ def convert_annotation(year, image_id, list_file):
 wd = getcwd()
 
 for year, image_set in sets:
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+    image_ids = open('../datasets/VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
     list_file = open('%s_%s.txt'%(year, image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
+        list_file.write('../datasets/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(year, image_id))
         convert_annotation(year, image_id, list_file)
         list_file.write('\n')
     list_file.close()
